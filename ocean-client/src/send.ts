0c9f3e65cd445c5959cde03b33163e1971ad49eb
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2022-10-15 20:45:28 +0200

    Update send.ts

--- ocean-client/src/send.ts
+++ ocean-client/src/send.ts
@@ -1,4 +1,3 @@
-import { CommonProgram, ProgramState } from './programs/common-program'
 import { SendProgramm } from './programs/send-program'
 import { StoreAWSSend } from './utils/store_aws_send'
 import { Telegram } from './utils/telegram'
@@ -23,12 +22,10 @@ export async function main(event: maxiEvent, context: any): Promise<Object> {
       console.log('received event ' + JSON.stringify(event))
     }
     const logId = process.env.VAULTMAXI_LOGID ? ' ' + process.env.VAULTMAXI_LOGID : ''
-    const telegram = new Telegram(settings, '[Reinvest' + settings.paramPostFix + ' ' + VERSION + logId + ']')
+    const telegram = new Telegram(settings, '[Send' + settings.paramPostFix + ' ' + VERSION + logId + ']')
 
-    let commonProgram: CommonProgram | undefined
     try {
       const program = new SendProgramm(store, new WalletSetup(settings, ocean))
-      commonProgram = program
       await program.init()
 
       if (event) {
@@ -38,97 +35,10 @@ export async function main(event: maxiEvent, context: any): Promise<Object> {
         }
       }
       let balances = await program.getTokenBalances()
-      const DFIinAddress = new BigNumber(balances.get('DFI')?.amount ?? 0)
-      let result = true
-
       await program.doSend(balances, telegram)
-
-      if (settings.stateInformation.state !== ProgramState.Idle) {
-        const information = settings.stateInformation
-        console.log('last execution stopped state ' + information.state)
-        console.log(' at tx ' + information.tx)
-        console.log(' with txId ' + information.txId)
-        console.log(' on block height ' + information.blockHeight)
-
-        if (information.state === ProgramState.WaitingForTransaction || information.txId.length > 0) {
-          console.log('waiting for tx from previous run')
-          const resultFromPrevTx = await program.waitForTx(information.txId, information.blockHeight)
-          balances = await program.getTokenBalances()
-          pool = await program.getPool(program.lmPair)
-          console.log(resultFromPrevTx ? 'done' : ' timed out -> cleanup')
-          let retryAdd = false
-          if (!resultFromPrevTx) {
-            if (information.tx == LMReinvestProgramTransaction.Swap) {
-              //was waiting for initial, just restart
-              information.state = ProgramState.Idle
-            } else {
-              retryAdd = true
-            }
-          } else {
-            if (information.tx == LMReinvestProgramTransaction.Swap) {
-              retryAdd = true
-            } else {
-              //was waiting for final addLiquidity, now there, so back to idle
-              information.state = ProgramState.Idle
-            }
-          }
-          if (retryAdd) {
-            information.state = ProgramState.Idle
-            const [usedAssetA, usedAssetB] = await program.addLiquidityWithFullWallet(pool!, balances, telegram)
-            if (usedAssetA !== undefined && usedAssetB !== undefined) {
-              const tokenA = pool!.tokenA
-              const tokenB = pool!.tokenB
-              await telegram.send(
-                'invested ' +
-                  usedAssetA.toFixed(8) +
-                  '@' +
-                  tokenA.symbol +
-                  ' paired with ' +
-                  usedAssetB.toFixed(8) +
-                  '@' +
-                  tokenB.symbol +
-                  ' after timeout in initial try',
-              )
-              console.log('done retry addLiquidity')
-            }
-          }
-          await program.updateToState(information.state, LMReinvestProgramTransaction.None)
-        }
-      }
-      console.log('starting with ' + DFIinAddress.toFixed(4) + ' in address')
-      await program.checkAndDoReinvest(pool!, balances, telegram)
-      await program.updateToState(ProgramState.Idle, LMReinvestProgramTransaction.None)
-      await telegram.log('executed script with ' + DFIinAddress.toFixed(4) + ' DFI in address')
-      console.log('script done ')
-      return { statusCode: result ? 200 : 500 }
-    } catch (e) {
-      console.error('Error in script')
-      console.error(e)
-      let message = 'There was an unexpected error in the script. please check the logs'
-      if (e instanceof WhaleClientTimeoutException) {
-        message = 'There was a timeout from the ocean api. will try again.'
-        //TODO: do we have to go to error state in this case? or just continue on current state next time?
-      }
-      if (!isNullOrEmpty(telegram.chatId) && !isNullOrEmpty(telegram.token)) {
-        await telegram.send(message)
-      } else {
-        await telegram.log(message)
-      }
-      if (ocean != undefined) {
-        console.info('falling back to default ocean')
-        ocean = undefined
-      }
-      //program might not be there, so directly the store with no access to ocean
-      await store.updateToState({
-        state: ProgramState.Error,
-        tx: '',
-        txId: commonProgram?.pendingTx ?? '',
-        blockHeight: 0,
-        version: VERSION,
-      })
-      await delay(60000) // cooldown and not to spam telegram
+      return { statusCode: 200 }
+    } catch {
+      return { statusCode: 500 }
     }
   }
-
-  return { statusCode: 500 }
 }
